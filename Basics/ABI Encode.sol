b25d9086592efa40131fc2fdba42f8da8ef03148
DMind <dminddev01@gmail.com>
2023-09-03 22:17:20 +0330

    Implement some basics code about inheritance and ABI Encoding and Decoding

---
+++ Basics/ABI Encode.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+interface IERC20 {
+    function transfer(address, uint) external;
+}
+
+contract Token {
+    function transfer(address, uint) external {}
+}
+
+contract AbiEncode {
+    function test(address _contract, bytes calldata data) external {
+        (bool ok, ) = _contract.call(data);
+        require(ok, "call failed");
+    }
+
+    function encodeWithSignature(address to, uint amount)
+        external
+        pure
+        returns (bytes memory)
+    {
+        // Typo is not checked - "transfer(address, uint)"
+        return abi.encodeWithSignature("transfer(address,uint256)", to, amount);
+    }
+
+    function encodeWithSelector(address to, uint amount)
+        external
+        pure
+        returns (bytes memory)
+    {
+        // Type is not checked - (IERC20.transfer.selector, true, amount)
+        return abi.encodeWithSelector(IERC20.transfer.selector, to, amount);
+    }
+
+    function encodeCall(address to, uint amount) external pure returns (bytes memory) {
+        // Typo and type errors will not compile
+        return abi.encodeCall(IERC20.transfer, (to, amount));
+    }
+}
